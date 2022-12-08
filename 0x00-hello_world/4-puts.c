7d5626cc73bc8483903a31b8283c08e657501c1b
FmpCodes <akorsunday070@gmail.com>
2022-12-08 09:53:38 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
+ * prints the phrase Programming is like building a multilingual puzzle
  *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  *
- *   * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
-
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
