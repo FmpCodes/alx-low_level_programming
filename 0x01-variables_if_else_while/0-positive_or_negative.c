0d04c9dbbd21e4c52314183cf3fb63a5435c9fcd
FmpCodes <akorsunday070@gmail.com>
2022-12-09 07:37:35 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,17 +3,16 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- * main - assigns a random number to int n everytime
- *
- * it executes, and prints it
- *
- * Return: Always 0 (Success)
+ * Return: 0 (success)
  *
  */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
